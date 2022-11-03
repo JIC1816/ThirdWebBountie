8097f8aaf7c8a2598d628941d2b7a491f0f57f6e
JIC1816 <puca.alejandro@gmail.com>
2022-11-03 18:57:50 -0300

    Custom features added to the template from the tutorial

--- contracts/Contract.sol
+++ contracts/Contract.sol
@@ -21,6 +21,12 @@ contract Contract is ERC721Base, PermissionsEnumerable {
 
     /** State variables */
 
+    uint256 public _price = 0.01 ether;
+    bool public _paused;
+    uint256 public maxTokenIds = 10;
+    uint256 public tokenIds;
+    string _baseTokenURI;
+
     mapping(uint256 => uint256) public powerLevel;
 
     /** Functions */
@@ -42,7 +48,42 @@ contract Contract is ERC721Base, PermissionsEnumerable {
         powerLevel[tokenId] = tokenId;
     }
 
-    function burn(uint256 _tokenId) public virtual override {
-        burn(_tokenId);
+    // This is an important feature if you want to prevent a security problem from escalating.
+
+    modifier onlyWhenNotPaused() {
+        require(
+            !_paused,
+            "This contract is currently in pause. Please wait until we fix the issue."
+        );
+        _;
     }
+
+    // This function allows anyone to get the contract balance.
+
+    function getContractBalance() public view returns (uint) {
+        return address(this).balance;
+    }
+
+    // In case of emergency, we can pause the contract with this function.
+
+    function setPaused(bool val) public onlyRole(DEFAULT_ADMIN_ROLE) {
+        _paused = val;
+    }
+
+    // This function uses onlyRole modifier and is useful to withdraw the ether obtained from minting. As you can see
+    // only the Admin can withdraw the ether.
+
+    function withdraw() public onlyRole(DEFAULT_ADMIN_ROLE) {
+        address _owner = owner();
+        uint256 amount = address(this).balance;
+        (bool sent, ) = _owner.call{value: amount}("");
+        require(sent, "Failed to send Ether");
+    }
+
+    // As Solidity by example explains, this functions are required if some people make errors when sending transactions.
+    // But also this functions are useful if someone wants to donate ETH directly to the contract without calling a function.
+
+    receive() external payable {}
+
+    fallback() external payable {}
 }
